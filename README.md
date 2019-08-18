5f7fe2ac72fcfd778f8cfc330c8ab167bce4772e
lucky091285 <53917426+lucky091285@users.noreply.github.com>
2019-08-18 22:15:32 +0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # ds33vladimir.github.io
+# v0.0.1
---
+++ script.js
@@ -0,0 +1,6 @@
+const name = document.querySelector('#name');
+const email = document.querySelector('#email');
+const tel = document.querySelector('#tel');
+const url = document.querySelector('#url');
+
+const submit = document.querySelector('.form__button');
